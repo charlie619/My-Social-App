51639a6f1babbae7cc017f112309469dc9aa5f69
charlie619 <gurpreet.charlie@gmail.com>
2019-10-24 20:14:53 +0100

    Edit Method for Activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -37,5 +37,12 @@ namespace API.Controllers
         {
             return await _mediator.Send(command);
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid Id, Edit.Command command)
+        {
+            command.Id = Id;
+            return await _mediator.Send(command);
+        }
     }
 }
